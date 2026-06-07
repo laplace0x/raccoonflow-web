4fe0294e3b66bf7c80284fb24b7f21617ee2baf1
laplace <laplace.agent@gmail.com>
2026-06-07 10:38:30 +0800

    Redirect www domain to apex

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typedRoutes: true
+  typedRoutes: true,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.raccoonflow.ai"
+          }
+        ],
+        destination: "https://raccoonflow.ai/:path*",
+        permanent: true
+      }
+    ];
+  }
 };
 
 export default nextConfig;
